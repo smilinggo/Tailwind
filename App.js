eaab7f5d082987c0ecc33633b983e95e6e30f161
Toney Tate <tdatate@gmail.com>
2022-08-26 09:08:05 -0400

    Getting Tailwind + React displaying

---
+++ App.js
@@ -0,0 +1,9 @@
+function LittleApp() {
+    return (
+        <h1 class="bg-green-300 m-4 p-2">
+            Hello World
+        </h1>
+    );
+}
+  
+  ReactDOM.render(<LittleApp />, document.getElementById("root"));
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+            './index.html',
+          './App.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
